36979fcfeadf4848ea9f81c44eee56ea5de4108c
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-06-03 15:07:47 +0100

    type of expense model created

---
+++ backend/models/TypeDepense.js
@@ -0,0 +1,26 @@
+// models/TypeDepense.js
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "TypeDepense",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nom: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "types_depenses",
+      timestamps: false,
+    }
+  );
+};
