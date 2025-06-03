28c75a93a5c709e7398425e265c11ddc363221c8
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-06-03 15:07:21 +0100

    users model created

---
+++ backend/models/User.js
@@ -0,0 +1,60 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nomComplete: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "nom_complete",
+      },
+      courriel: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      motDePasseHache: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "mot_de_passe_hache",
+      },
+      possedeVoiturePersonnelle: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+        field: "possede_voiture_personnelle",
+      },
+      estActif: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+        field: "est_actif",
+      },
+      dateCreation: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+        field: "date_creation",
+      },
+      dateModification: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+        field: "date_modification",
+      },
+    },
+    {
+      tableName: "utilisateurs",
+      timestamps: false,
+      hooks: {
+        beforeUpdate: (user) => {
+          user.dateModification = new Date();
+        },
+      },
+    }
+  );
+};
