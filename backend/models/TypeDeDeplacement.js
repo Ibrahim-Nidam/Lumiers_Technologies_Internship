b8e984cb07f1897d64b859933adafa6043f39f28
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-06-04 10:22:13 +0100

    type of displacement updated

--- backend/models/TypeDeDeplacement.js
+++ backend/models/TypeDeDeplacement.js
@@ -1,25 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define(
-    "TypeDeDeplacement",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      nom: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      description: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-    },
-    {
-      tableName: "types_de_deplacement",
-      timestamps: false,
-    }
-  );
+  return sequelize.define("TypeDeDeplacement", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nom: { type: DataTypes.STRING, allowNull: false, unique: true },
+    description: { type: DataTypes.STRING, allowNull: true },
+  }, {
+    tableName: "types_de_deplacement",
+    timestamps: false,
+  });
 };
